63f90422301268b325c899e5f66f443ca89e6315
aya <56827395+almayra@users.noreply.github.com>
2020-12-23 19:16:26 +0700

    [DSM-6]: story control for typography

--- src/components/Typography/Typography.js
+++ src/components/Typography/Typography.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import "./Typography.css"
+import PropTypes from "prop-types"
 
-function Typography (props) {
-	const { variant = "primary", bold, children, ...rest}= props
+export const Typography = ({variant, bold, label, ...rest})=>{
 	return(
 		<div className={`typography ${variant} ${bold ? "bold" : ""}`} {...rest}>
-			{children}
+			{label}
 		</div>
 	)
 }
 
-export default Typography
+Typography.propTypes = {
+	variant: PropTypes.oneOf([
+		'metadata-01', 
+		'metadata-02', 
+		'body-short-01', 
+		'body-short-02', 
+		'body-long-01', 
+		'body-long-02', 
+		'heading-01', 
+		'heading-02', 
+		'heading-03', 
+		'heading-04', 
+		'heading-05', 
+		'heading-06', 
+		'heading-07', 
+		'heading-08',
+	]),
+	bold: PropTypes.bool,
+	label: PropTypes.string.isRequired
+}
+
+Typography.defaultProps = {
+	variant: 'body-short-01',
+	bold: false,
+}
--- src/components/Typography/typography.stories.js
+++ src/components/Typography/typography.stories.js
@@ -1,26 +1,15 @@
 import React from 'react'
-import Typography from "./Typography"
+import {Typography} from "./Typography"
 
 export default{
 	title: "Typography",
 	component: Typography
 }
 
-export const metadata01 = () => <Typography variant="metadata-01">Metadata 01</Typography>
-export const metadata02 = () => <Typography variant="metadata-02">Metadata 02</Typography>
-export const bodyShort01 = () => <Typography variant="body-short-01">Body Short 01</Typography>
-export const bodyShortBold01 = () => <Typography variant="body-short-01 bold">Body Short 01</Typography>
-export const bodyShort02 = () => <Typography variant="body-short-01">Body Short 02</Typography>
-export const bodyShortBold02 = () => <Typography variant="body-short-01 bold">Body Short 02</Typography>
-export const bodyLong01 = () => <Typography variant="body-long-01">Body Long 01</Typography>
-export const bodyLongBold01 = () => <Typography variant="body-long-01 bold">Body Long 01</Typography>
-export const bodyLong02 = () => <Typography variant="body-long-02">Body Long 02</Typography>
-export const bodyLongBold02 = () => <Typography variant="body-long-02 bold">Body Long 02</Typography>
-export const heading01 = () => <Typography variant="heading-01">Heading 01</Typography>
-export const heading02 = () => <Typography variant="heading-02">Heading 02</Typography>
-export const heading03 = () => <Typography variant="heading-03">Heading 03</Typography>
-export const heading04 = () => <Typography variant="heading-04">Heading 04</Typography>
-export const heading05 = () => <Typography variant="heading-05">Heading 05</Typography>
-export const heading06 = () => <Typography variant="heading-06">Heading 06</Typography>
-export const heading07 = () => <Typography variant="heading-07">Heading 07</Typography>
-export const heading08 = () => <Typography variant="heading-08">Heading 08</Typography>
+const Template = (args) => <Typography {...args}/>
+
+export const TextStyles = Template.bind({});
+TextStyles.args = {
+	variant: "body-short-01",
+	label: "The quick brown fox jumps over the lazy dog"
+}
